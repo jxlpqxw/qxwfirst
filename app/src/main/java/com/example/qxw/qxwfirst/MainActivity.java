04e2ed87e30303f04bd61742f755b1490c362c7d
jxlpqxw <985254679@qq.com>
2016-09-21 09:59:45 +0800

    init project

---
+++ app/src/main/java/com/example/qxw/qxwfirst/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.qxw.qxwfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by qxw on 2016/9/21.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
