feedf441f2f6265b997de2c91d86cee4c1dcf624
jxlpqxw <985254679@qq.com>
2016-10-20 17:29:10 +0800

    10.20

---
+++ app/src/main/java/com/example/qxw/qxwfirst/selcet_city.java
@@ -0,0 +1,15 @@
+package com.example.qxw.qxwfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by qxw on 2016/10/18.
+ */
+public class selcet_city extends Activity {
+    @Override
+    protected void onCreate(Bundle savedIstanceState){
+        super.onCreate(savedIstanceState);
+        setContentView(R.layout.select_city);
+    }
+}
